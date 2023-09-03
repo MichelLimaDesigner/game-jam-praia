bd9ca434a7a8fa5702c893298d85a98dc8642318
Pigor <higorribeirosantos@gmail.com>
2023-09-03 02:11:29 -0300

    Wip: ajust in sripts and enemies

--- Assets/Scripts/Enemy/floatEnemy.cs
+++ Assets/Scripts/Enemy/floatEnemy.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class FloatEnemy : MonoBehaviour
 {
     [SerializeField] private Transform[] points;
@@ -62,3 +64,4 @@ public class FloatEnemy : MonoBehaviour
         }
     }
 }
+
